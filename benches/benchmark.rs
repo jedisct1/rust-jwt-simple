90233a445a8822f509570b2c0c2835d163095c0e
Frank Denis <github@pureftpd.org>
2022-02-24 15:50:21 +0100

    Add benchmarks

---
+++ benches/benchmark.rs
@@ -0,0 +1,32 @@
+use benchmark_simple::*;
+use jwt_simple::prelude::*;
+
+fn main() {
+    let bench = Bench::new();
+
+    let options = &Options {
+        iterations: 1000,
+        warmup_iterations: 100,
+        min_samples: 5,
+        max_samples: 10,
+        max_rsd: 1.0,
+        ..Default::default()
+    };
+
+    let kp = RS256KeyPair::generate(2048).unwrap();
+    let pk = kp.public_key();
+
+    let claims = Claims::create(Duration::from_hours(2));
+
+    let token = kp.sign(claims.clone()).unwrap();
+    let res = bench.run(options, move || {
+        let token = kp.sign(claims.clone()).unwrap();
+        token
+    });
+    println!("rsa-2048 - sign: {}", res.throughput(1));
+
+    let res = bench.run(options, move || {
+        pk.verify_token::<NoCustomClaims>(&token, Default::default())
+    });
+    println!("rsa-2048 - verify: {}", res.throughput(1));
+}
