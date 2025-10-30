1f1b47d30492011f3d4e96adc4235fa99941fa11
Frank Denis <github@pureftpd.org>
2025-10-30 22:26:13 +0100

    Typo

--- src/token.rs
+++ src/token.rs
@@ -221,7 +221,7 @@ impl Token {
         Ok(claims)
     }
 
-    /// Decode token information that can be usedful prior to signature/tag
+    /// Decode token information that can be useful prior to signature/tag
     /// verification
     pub fn decode_metadata(token: &str) -> Result<TokenMetadata, Error> {
         let mut parts = token.split('.');
